68e9f5c23c7f1f5509637637f121da8a60f538ca
suhas <suhas.yarrabothu@gmail.com>
2017-12-23 00:23:33 +0530

    first example

--- week/hello.py
+++ week/hello.py
@@ -1,5 +1,5 @@
-x=2+2
-print(x)
+# x=2+2
+# print(x)
 
 class utils:
     def add(a,b):
@@ -11,8 +11,17 @@ class utils:
     def mean(arr):
         return sum(arr)/len(arr)
 
+    def fibonaci(n):
+        a, b = 0, 1
+        while (b < n):
+            print(b)
+            a, b = b, a + b
+
+
+#
 if __name__ == '__main__':
     u =utils
-    print(u.add(2,3))
-    print(u.square(5))
-    print(u.mean([5,32,5]))
+    # print(u.add(2,3))
+    # print(u.square(5))
+    # print(u.mean([5,32,5]))
+    print(u.fibonaci(8))
