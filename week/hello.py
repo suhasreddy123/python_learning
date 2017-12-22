7ed39cd67f75a8955c389fff3a9ef6998366131e
suhas <suhas.yarrabothu@gmail.com>
2017-12-22 23:41:24 +0530

    first example

--- week/hello.py
+++ week/hello.py
@@ -1,16 +1,18 @@
 x=2+2
 print(x)
 
-def add(a,b):
-    return a+b
+class utils:
+    def add(a,b):
+        return a+b
 
-def square(x):
-    return x**2
+    def square(x):
+        return x**2
 
-def mean(arr):
-    return sum(arr)/len(arr)
+    def mean(arr):
+        return sum(arr)/len(arr)
 
 if __name__ == '__main__':
-    print(add(2,3))
-    print(square(5))
-    print(mean([5,32,5]))
+    u =utils
+    print(u.add(2,3))
+    print(u.square(5))
+    print(u.mean([5,32,5]))
